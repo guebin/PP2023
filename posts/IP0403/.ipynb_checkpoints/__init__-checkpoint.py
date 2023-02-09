a980f5b779feca8393ea44228c55c1a3d0f455fe
cgb2 <miruetoto@gmail.com>
2023-02-09 23:00:26 +0900

    .

---
+++ posts/IP0403/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,1 @@
+from . import myfuns2
---
+++ posts/myfuns.py
@@ -0,0 +1,6 @@
+"""이것은 길이가 2인 벡터의 합 혹은 차를 구하는 모듈입니다.""" 
+def vec2_add(a,b): 
+    print("이것은 myfuns.py에 정의된 함수입니다") 
+    return [a[0]+b[0], a[1]+b[1]]
+def vec2_sub(a,b): 
+    return [a[0]-b[0], a[1]-b[1]]
